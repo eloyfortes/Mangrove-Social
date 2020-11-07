6dc3eae90e01c953913c55d38fd981badafcf44e
Filipe dos Santos Gundim <filipe@admooh.com>
2020-11-07 16:30:58 -0300

    switch login to index

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {StyleSheet, TextInput, Text, View, Button} from 'react-native';
+import {StyleSheet, TextInput, View, Button} from 'react-native';
 
 import { Input } from 'react-native-elements';
 
